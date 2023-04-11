63a30731d63f1ebc6c2ed18bc95ececf174a43ba
deergod <CommonTroll@live.co.uk>
2023-04-11 11:35:31 +0100

    following examples

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/music", express.static("music"));
+app.use("/music/about", express.static("music/about.html"));
 app.use("/books", express.static("books"));
 app.use("/", express.static("anotherRoute"));
 
