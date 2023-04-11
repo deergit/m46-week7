b1026325297cc7608acca2f66cc47b073f0ed0ea
deergod <CommonTroll@live.co.uk>
2023-04-11 14:51:52 +0100

    following examples (kinda)

--- src/server.js
+++ src/server.js
@@ -7,31 +7,35 @@ const app = express();
 // app.use("/books", express.static("books"));
 // app.use("/", express.static("anotherRoute"));
 
+let bookList = [{
+  title: "Lord of the Rings",
+  author: "J.R.R. Tolkein",
+  genre: "Fantasy"
+}]
+
 app.use(express.json());
 
-app.get("/book", (request, response) => {
-  const book = {
-    title: "Lord of the Rings",
-    author: "J.R.R. Tolkein",
-    genre: "Fantasy"
-  }
-
+app.get("/books", (request, response) => {
   const successResponse = {
     message: "Response sent successfully",
-    book: book
+    books: bookList
   }
 
   response.send(successResponse);
-})
+});
 
-app.get("/anotherroute", (request, response) => {
-  response.send("Hello from another route");
-})
+app.post("/books", (request, response) => {
+  console.log(`incoming data: ${request.body}`);
 
-app.post("/book", (request, response) => {
-  console.log(request.body);
+  const newBook = {
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre
+  }
+
+  bookList.push(newBook);
 
   response.send("Data received");
-})
+});
 
 app.listen(5001, () => console.log("Listen server open"));
