1d929f535bc496ebe6caebac8fd2f19af7412ba5
deergod <CommonTroll@live.co.uk>
2023-04-11 10:57:52 +0100

    following examples

--- src/server.js
+++ src/server.js
@@ -2,5 +2,6 @@ const express = require("express");
 const app = express();
 
 app.use("/books", express.static("books"));
+app.use("/", express.static("anotherRoute"));
 
 app.listen(5001, () => console.log("Listen server open"));
