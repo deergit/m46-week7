1b3ab125b00a7d8214f68bb8f339f9b1bc17e43f
deergod <CommonTroll@live.co.uk>
2023-04-11 15:52:57 +0100

    working on additional tasks

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const app = express();
 // app.use("/", express.static("anotherRoute"));
 
 let bookList = [{
+  id: 1,
   title: "Lord of the Rings",
   author: "J.R.R. Tolkein",
   genre: "Fantasy"
@@ -25,17 +26,47 @@ app.get("/books", (request, response) => {
 });
 
 app.post("/books", (request, response) => {
-  console.log(`incoming data: ${request.body}`);
-
   const newBook = {
+    id: bookList.length + 1,
     title: request.body.title,
     author: request.body.author,
     genre: request.body.genre
   }
 
   bookList.push(newBook);
-
   response.send("Data received");
+});
+
+app.put("/books/:id", (request, response) => {
+  const match = bookList.find(book => book.id === parseInt(request.params.id));
+
+  if (match) {
+    const updateBook = {
+      id: match.id,
+      title: request.body.title,
+      author: request.body.author,
+      genre: request.body.genre
+    }
+
+    match.title = updateBook.title ? updateBook.title : match.title;
+    match.author = updateBook.author ? updateBook.author : match.author;
+    match.genre = updateBook.genre ? updateBook.genre : match.genre;
+
+    response.send("Entry updated");
+  } else {
+    response.sendStatus(400);
+  }
+});
+
+app.delete("/books/:id", (request, response) => {
+  const match = bookList.find(book => book.id === parseInt(request.params.id));
+
+  if (match) {
+    bookList = bookList.filter(book => book.id !== match.id);
+    response.send("Entry deleted");
+  } else {
+    response.sendStatus(400);
+  }
 });
 
 app.listen(5001, () => console.log("Listen server open"));
