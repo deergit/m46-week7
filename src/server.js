62e14cd4d64387887419f6791b02add2dc769402
deergod <CommonTroll@live.co.uk>
2023-04-17 15:23:58 +0100

    updating packages and server js to add cors

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 require("./db/connection");
 
 const express = require("express");
+const cors = require("cors");
 const bookRouter = require("./modbooks/routes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(bookRouter);
 
